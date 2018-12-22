6d3d8f9d2be7f7534a9ff2f2dbf8d7c4d77557eb
KajKas <kajetankasperuk@gmail.com>
2018-12-22 15:04:59 +0100

    positioned main elements

--- src/LogIn.js
+++ src/LogIn.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import LogForm from './LogForm';
+import './App.css'
 
 class LogIn extends Component {
   state = {
@@ -7,12 +8,13 @@ class LogIn extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Get tracking</h1>
-        <h4>Log in to your Toggl account.</h4>
-        <LogForm button1={"Log in"} button2={"Log in with Google"}/>
-
-      </div>
+        <main className="main">
+          <div className="form-container">
+            <h1>Get tracking</h1>
+            <h4>Log in to your Toggl account.</h4>
+            <LogForm button1={"Log in"} button2={"Log in with Google"}/>
+          </div>
+        </main>
     )
   }
 }
--- src/SignUp.js
+++ src/SignUp.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import LogForm from './LogForm';
+import './App.css'
 
 class SignUp extends Component {
   state = {
@@ -7,12 +8,13 @@ class SignUp extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Let's get started</h1>
-        <h4>Kickstart your productivity with Toggl.</h4>
-        <LogForm button1={"Sign in"} button2={"Sign in with Google"}/>
-
-      </div>
+        <main className="main">
+          <div className="form-container">
+            <h1>Let's get started</h1>
+            <h4>Kickstart your productivity with Toggl.</h4>
+            <LogForm button1={"Sign in"} button2={"Sign in with Google"}/>
+          </div>
+        </main>
     )
   }
 }
