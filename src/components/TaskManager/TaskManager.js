213d26cd8463111ad2007b348acb85e44a525950
KajKas <kajetankasperuk@gmail.com>
2018-12-28 19:22:25 +0100

    looks better, now the svgs

--- src/components/TaskManager/TaskManager.js
+++ src/components/TaskManager/TaskManager.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './TaskManager.css';
 import Timer from '../Timer/Timer';
@@ -21,56 +21,60 @@ class TaskManager extends Component {
   render() {
     return (
       <Router>
-            <div className="side-bar">
-                <header className="task-mamager_header">
-                    <ul className="task-mamager_menu">            
-                        <li>
-                            <Link to="/timer">Timer</Link>
-                        </li>
-                        <li>
-                            <Link to="/dashboard">Dashboard</Link>
-                        </li>
-                        <li>
-                            <Link to="/reports">Reports</Link>
-                        </li>
-                        <li>
-                            <Link to="/insights">Insights</Link>
-                        </li>
-                        <li>
-                            <Link to="/savedreports">Saved Reports</Link>
-                        </li>
-                        <li>
-                            <Link to="/projects">Projects</Link>
-                        </li>
-                        <li>
-                            <Link to="/clients">Clients</Link>
-                        </li>
-                        <li>
-                            <Link to="/team">Team</Link>
-                        </li>
-                        <li>
-                            <Link to="/workspaces">Workspaces</Link>
-                        </li>
-                        <li>
-                            <Link to="/tags">Tags</Link>
-                        </li>
-                        <li>
-                            <Link to="/help">Help</Link>
-                        </li>
-                    </ul>                    
-                </header>
-                <Route path="/timer" component={Timer}/>
-                <Route path="/dashboard" component={Dashboard}/>
-                <Route path="/reports" component={Reports}/>
-                <Route path="/insights" component={Insights}/>
-                <Route path="/savedreports" component={SavedReports}/>
-                <Route path="/projects" component={Projects}/>
-                <Route path="/clients" component={Clients}/>
-                <Route path="/team" component={Team}/>
-                <Route path="/workspaces" component={Workspaces}/>
-                <Route path="/tags" component={Tags}/>
-                <Route path="/help" component={Help}/>
-            </div>
+            <Fragment>
+                <div className="side-bar">
+                    <header className="task-mamager_header">
+                        <ul className="task-mamager_menu">            
+                            <li>
+                                <Link to="/timer">Timer</Link>
+                            </li>
+                            <li>
+                                <Link to="/dashboard">Dashboard</Link>
+                            </li>
+                            <li>
+                                <Link to="/reports">Reports</Link>
+                            </li>
+                            <li>
+                                <Link to="/insights">Insights</Link>
+                            </li>
+                            <li>
+                                <Link to="/savedreports">Saved Reports</Link>
+                            </li>
+                            <li>
+                                <Link to="/projects">Projects</Link>
+                            </li>
+                            <li>
+                                <Link to="/clients">Clients</Link>
+                            </li>
+                            <li>
+                                <Link to="/team">Team</Link>
+                            </li>
+                            <li>
+                                <Link to="/workspaces">Workspaces</Link>
+                            </li>
+                            <li>
+                                <Link to="/tags">Tags</Link>
+                            </li>
+                            <li>
+                                <Link to="/help">Help</Link>
+                            </li>
+                        </ul>                    
+                    </header>                
+                </div>
+                <div className="task-manager_main">
+                    <Route path="/timer" component={Timer}/>
+                    <Route path="/dashboard" component={Dashboard}/>
+                    <Route path="/reports" component={Reports}/>
+                    <Route path="/insights" component={Insights}/>
+                    <Route path="/savedreports" component={SavedReports}/>
+                    <Route path="/projects" component={Projects}/>
+                    <Route path="/clients" component={Clients}/>
+                    <Route path="/team" component={Team}/>
+                    <Route path="/workspaces" component={Workspaces}/>
+                    <Route path="/tags" component={Tags}/>
+                    <Route path="/help" component={Help}/>
+                </div>
+            </Fragment>
         </Router>
     )
   }
